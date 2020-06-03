95610c3982537c8530ad17a64d703513a4a24da5
Virgilio Pierini <virgilio.pierini@clickode.it>
2020-06-03 17:28:39 +0200

    Setup of debug firmware for showing messages

--- main.ts
+++ main.ts
@@ -1,24 +1,10 @@
-let new5 = 0
 MbitMore.startService()
-MbitMore.setSharedData(SharedDataIndex.DATA5, 0)
-let old5 = 0
 basic.forever(function () {
-    new5 = MbitMore.getSharedData(SharedDataIndex.DATA5)
-    if (new5 != old5) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . # . .
-            . # . # .
-            . . . . .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+    if (MbitMore.getSharedData(SharedDataIndex.DATA0) == 21) {
+        basic.showString("Led1:" + MbitMore.getSharedData(SharedDataIndex.DATA1) + "-" + MbitMore.getSharedData(SharedDataIndex.DATA2) + "-" + MbitMore.getSharedData(SharedDataIndex.DATA3))
+        MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
+    } else if (MbitMore.getSharedData(SharedDataIndex.DATA0) == 31) {
+        basic.showString("Direzione " + MbitMore.getSharedData(SharedDataIndex.DATA1) + " e Delay " + MbitMore.getSharedData(SharedDataIndex.DATA2))
+        MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
     }
 })
