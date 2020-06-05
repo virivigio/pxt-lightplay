d3ca6c6403d6a32889fc1a8ce52da2f7a9bd0624
Virgilio Pierini <virgilio.pierini@clickode.it>
2020-06-05 15:07:26 +0200

    First working prototype with single slot messages. JS only

--- main.ts
+++ main.ts
@@ -1,10 +1,46 @@
+let slot0 = 0
+let comando = 0
+
+let direzione = 0     //Start-Stop
+let delay = 0         //Start-Stop
+let passi = 0         //Stepper
+
+let ledRGB = 0
+let R = 0
+let G = 0
+let B = 0
+
 MbitMore.startService()
+
 basic.forever(function () {
-    if (MbitMore.getSharedData(SharedDataIndex.DATA0) == 21) {
-        basic.showString("Led1:" + MbitMore.getSharedData(SharedDataIndex.DATA1) + "-" + MbitMore.getSharedData(SharedDataIndex.DATA2) + "-" + MbitMore.getSharedData(SharedDataIndex.DATA3))
+    slot0 = MbitMore.getSharedData(SharedDataIndex.DATA0)
+    comando = slot0 >> 12
+
+    if (comando == 4) { //Start-Stop
+        direzione = (slot0 >> 8) & 3
+        if (direzione == 0) delay=0; //ignore code, its a stop
+        else delay = slot0 & 255
+        basic.showString("Start:" + direzione + ":" + delay)
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
-    } else if (MbitMore.getSharedData(SharedDataIndex.DATA0) == 31) {
-        basic.showString("Direzione " + MbitMore.getSharedData(SharedDataIndex.DATA1) + " e Delay " + MbitMore.getSharedData(SharedDataIndex.DATA2))
+    }
+    else if (comando == 7) { //Stepper
+        passi = slot0 & 2047
+        basic.showString("Passi:" + passi)
+        MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
+    }
+    else if ((comando == 1) || (comando == 2)) { //RGB
+        ledRGB = comando
+        R = (slot0 >> 8) & 15
+        G = (slot0 >> 4) & 15
+        B = (slot0 >> 0) & 15
+        basic.showString("Led:" + ledRGB + " R:" + R + " G:" + G + " B:" + B)
+        MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
+    }
+    else if (comando == 3) { //RGB all
+        R = (slot0 >> 8) & 15
+        G = (slot0 >> 4) & 15
+        B = (slot0 >> 0) & 15
+        basic.showString("Led All:" + " R:" + R + " G:" + G + " B:" + B)
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
     }
 })
