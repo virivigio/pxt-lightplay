5e423f5a2ec9fee7e49784d39fb8eefe83d8ee64
Virgilio Pierini <virgilio.pierini@clickode.it>
2020-07-06 22:30:33 +0200

    First working prototype with led and motor. JS only

--- main.ts
+++ main.ts
@@ -4,12 +4,15 @@ let comando = 0
 let direzione = 0     //Start-Stop
 let delay = 0         //Start-Stop
 let passi = 0         //Stepper
+let motorIsOn = 0
 
 let ledRGB = 0
 let R = 0
 let G = 0
 let B = 0
 
+
+Kitronik_Robotics_Board.setStepperMotorSteps(Kitronik_Robotics_Board.StepperMotors.Stepper1, 2048)
 MbitMore.startService()
 
 basic.forever(function () {
@@ -18,7 +21,7 @@ basic.forever(function () {
 
     if (comando == 4) { //Start-Stop
         direzione = (slot0 >> 8) & 3
-        if (direzione == 0) delay=0; //ignore code, its a stop
+        if (direzione == 0) delay = 0; //ignore code, its a stop
         else delay = slot0 & 255
         basic.showString("Start:" + direzione + ":" + delay)
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
@@ -42,5 +45,23 @@ basic.forever(function () {
         B = (slot0 >> 0) & 15
         basic.showString("Led All:" + " R:" + R + " G:" + G + " B:" + B)
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
+    }
+})
+
+basic.forever(function () {
+    if (direzione == 1) {
+        Kitronik_Robotics_Board.stepperMotorTurnAngle(Kitronik_Robotics_Board.StepperMotors.Stepper1, Kitronik_Robotics_Board.MotorDirection.Forward, 5)
+        motorIsOn = 1
+        basic.pause(delay)
+    } else if (direzione == 2) {
+        Kitronik_Robotics_Board.stepperMotorTurnAngle(Kitronik_Robotics_Board.StepperMotors.Stepper1, Kitronik_Robotics_Board.MotorDirection.Reverse, 5)
+        motorIsOn = 1
+        basic.pause(delay)
+    } else {
+        if (motorIsOn == 1) {
+            motorIsOn = 0
+            Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
+            Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor2)
+        }
     }
 })
