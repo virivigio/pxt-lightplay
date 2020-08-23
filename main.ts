29a3bd8c1086850a676ba8b800dbfb152fd6ce0b
Virgilio Pierini <virgilio.pierini@clickode.it>
2020-08-24 00:13:30 +0200

    Adds complete led management, Stepper to be verified

--- main.ts
+++ main.ts
@@ -11,8 +11,20 @@ let R = 0
 let G = 0
 let B = 0
 
+function debug(message:string) {
+    //basic.showString(message)
+}
 
-Kitronik_Robotics_Board.setStepperMotorSteps(Kitronik_Robotics_Board.StepperMotors.Stepper1, 2048)
+//check pin numbers
+//turn off Led 1 
+pins.analogWritePin(AnalogPin.P2, 0) //R
+pins.analogWritePin(AnalogPin.P1, 0) //G
+pins.analogWritePin(AnalogPin.P0, 0) //B
+//turn off Led 2
+//pins.analogWritePin(AnalogPin.P2, 0) //R
+//pins.analogWritePin(AnalogPin.P1, 0) //G
+//pins.analogWritePin(AnalogPin.P0, 0) //B 
+
 MbitMore.startService()
 
 basic.forever(function () {
@@ -20,15 +32,23 @@ basic.forever(function () {
     comando = slot0 >> 12
 
     if (comando == 4) { //Start-Stop
-        direzione = (slot0 >> 8) & 3
-        if (direzione == 0) delay = 0; //ignore code, its a stop
-        else delay = slot0 & 255
-        basic.showString("Start:" + direzione + ":" + delay)
+        //avoid race conditions
+        let localDirezione
+        let localDelay
+
+        localDirezione = (slot0 >> 8) & 3
+        if (localDirezione == 0) localDelay = 0; //ignore code, it's a stop
+        else localDelay = slot0 & 255
+        debug("Start:" + localDirezione + ":" + localDelay)
+     
+        delay = localDelay
+        direzione = localDirezione
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
     }
     else if (comando == 7) { //Stepper
         passi = slot0 & 2047
-        basic.showString("Passi:" + passi)
+        debug("Passi:" + passi)
+        Kitronik_Robotics_Board.setStepperMotorSteps(Kitronik_Robotics_Board.StepperMotors.Stepper1, passi)
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
     }
     else if ((comando == 1) || (comando == 2)) { //RGB
@@ -36,14 +56,31 @@ basic.forever(function () {
         R = (slot0 >> 8) & 15
         G = (slot0 >> 4) & 15
         B = (slot0 >> 0) & 15
-        basic.showString("Led:" + ledRGB + " R:" + R + " G:" + G + " B:" + B)
+        debug("Led:" + ledRGB + " R:" + R + " G:" + G + " B:" + B)
+        //check pin numbers
+        if (ledRGB == 1) {
+            pins.analogWritePin(AnalogPin.P2, R*64) //R
+            pins.analogWritePin(AnalogPin.P1, G*64) //G
+            pins.analogWritePin(AnalogPin.P0, B*64) //B
+        } else {
+            //pins.analogWritePin(AnalogPin.P2, R*64) //R
+            //pins.analogWritePin(AnalogPin.P1, G*64) //G
+            //pins.analogWritePin(AnalogPin.P0, B*64) //B            
+        }
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
     }
     else if (comando == 3) { //RGB all
         R = (slot0 >> 8) & 15
         G = (slot0 >> 4) & 15
         B = (slot0 >> 0) & 15
-        basic.showString("Led All:" + " R:" + R + " G:" + G + " B:" + B)
+        debug("Led All:" + " R:" + R + " G:" + G + " B:" + B)
+        //check pin numbers
+        pins.analogWritePin(AnalogPin.P2, R*64) //R
+        pins.analogWritePin(AnalogPin.P1, G*64) //G
+        pins.analogWritePin(AnalogPin.P0, B*64) //B
+        //pins.analogWritePin(AnalogPin.P2, R*64) //R
+        //pins.analogWritePin(AnalogPin.P1, G*64) //G
+        //pins.analogWritePin(AnalogPin.P0, B*64) //B 
         MbitMore.setSharedData(SharedDataIndex.DATA0, 0)
     }
 })
